9dcfbdaf2cb3767b93c90a004ea340a751a876a5
Juan Carlos Gutierrez <DiosJuanVero2015@gmail.com>
2024-01-15 21:30:58 -0300

    Proyecto Portfolio Imagenes

---
+++ demo.js
@@ -0,0 +1,21 @@
+// var suma = 0;
+
+// suma = suma +1;
+// console.log(suma);
+// suma = suma +1;
+// console.log(suma);
+// suma = suma +1;
+// console.log(suma);
+// suma = suma +1;
+
+
+//Mientras i sea menor o igual a 5, imprimira el valor de i en cada vuelta
+for (var i = 0; i<=5;i++){
+    console.log(i);
+}
+function cuentaRegresiva(num){
+    for(var i= num ;i>=0;i--) {
+        console.log(i);
+    }
+};
+cuentaRegresiva(10);
